9b2da7c922bb517f895cd2c65568c6603588c10d
Mark Mavromatis <markmavromatis@gmail.com>
2023-01-04 13:03:39 -0600

    Upgraded web3 dependency, Solidity5

--- truffle.js
+++ truffle.js
@@ -1,11 +1,11 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+var mnemonic = "essay aspect awful day work poem gate love bus slice retreat quality";
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:9545/", 0, 50);
       },
       network_id: '*',
       gas: 9999999
@@ -13,7 +13,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "^0.4.24"
+      version: "0.5.16"
     }
   }
 };
