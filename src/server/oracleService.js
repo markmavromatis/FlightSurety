90b392ed1a89a65077afa204d3dae3c0843deebb
Mark Mavromatis <markmavromatis@gmail.com>
2023-01-11 12:15:19 -0500

    Server - Added CORS, Moved oracle registration code to external class

---
+++ src/server/oracleService.js
@@ -0,0 +1,80 @@
+const REGISTRATION_FEE = 1; // 1 wei
+const ORACLE_COUNT = 20;
+
+export default class OracleService {
+
+    constructor(accounts, flightSuretyApp) {
+        this.oracles = [];
+        this.oraclesRegistered = false;
+        this.flightSuretyApp = flightSuretyApp;
+        const requiredAccountsLength = ORACLE_COUNT + 1
+        if (requiredAccountsLength > accounts.length) {
+          throw new Error(`Oracle registration requires access to at least ${requiredAccountsLength} accounts`);
+        }
+        for (let i = 0; i < ORACLE_COUNT; i++) {
+          let newOracle = {};
+          newOracle.address = accounts[i + 1];
+          this.oracles.push(newOracle);
+        }
+          
+    };
+
+    async registerOracles() {
+        console.log("Registering Oracles...");
+        for (let i = 0; i < this.oracles.length; i++) {
+            const oracle = this.oracles[i];
+            await this.flightSuretyApp.methods.registerOracle()
+            .send({ from: oracle.address, gas: 1000000, value: REGISTRATION_FEE}, (error, result) => {
+                if (error) {
+                throw new Error("Failed to register oracle. Reason: " + error);
+                }
+            });
+    
+            console.log("Registering indices for oracle " + (i + 1));
+            // Retrieve indexes
+            await this.flightSuretyApp.methods.getOracleIndexes()
+                .call({ from: oracle.address}, (error, result) => {
+                    if (error) {
+                    throw new Error("Failed to call getOracleIndexes()! Reason: " + error);
+                    }
+                    oracle.indexLookup = {};
+                    oracle.indexLookup[result[0]] = true;
+                    oracle.indexLookup[result[1]] = true;
+                    oracle.indexLookup[result[2]] = true;
+                })
+            console.log("Finished setting up oracle " + (i + 1) + " " + JSON.stringify(oracle.indexLookup));
+            this.oraclesRegistered = true;
+        }
+        
+    }
+
+    async registerOracleListeners() {
+      // Event listener for OracleRequest events
+      this.flightSuretyApp.events.OracleRequest()
+      .on('data', event => {
+
+        console.log("***** EVENT")
+        // Iterate over oracles and send updates for matches
+        const statusCode = 20;
+        const requestIndex = event.returnValues[0];
+        this.oracles.map(async (oracle, index) => {
+          if (oracle.indexLookup[requestIndex] == true) {
+
+            const requestAirline = event.returnValues[1];
+            const requestFlight = event.returnValues[2];
+            const requestTimestamp = event.returnValues[3];
+            const requestIndexAsNumber = parseInt(requestIndex);
+            await this.flightSuretyApp.methods.submitOracleResponse
+            (requestIndexAsNumber, requestAirline,requestFlight, requestTimestamp, statusCode)
+            .send({ from: oracle.address, gas: 100000}, (error, result) => {
+              if (error) {
+                throw new Error("Failed to call submitOracleResponse. Reason: " + error);
+              }
+            });
+          }
+        })
+
+      });
+
+    }
+}
--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,17 @@
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
 import Config from './config.json';
 import express from 'express';
-
+import cors from 'cors';
+import OracleService from './OracleService';
 
 let config = Config['localhost'];
 var Web3 = require('web3');
+
+let oracleService = null;
 const web3 = new Web3("ws://127.0.0.1:8555");
 
-const REGISTRATION_FEE = 1; // 1 wei
-const ORACLE_COUNT = 20;
-
 let oracles = [];
+let flightSuretyApp = null;
 
 web3.eth.net.isListening()
 .then(() => {
@@ -18,84 +19,44 @@ web3.eth.net.isListening()
 
     web3.eth.getAccounts().then((accounts) => {
       web3.eth.defaultAccount = accounts[0];
-      console.log("Account = " + web3.eth.defaultAccount);
       console.log("# accounts = " + accounts.length);
-      console.log("Checking connectivity with blockchain...");
-
-      // Initialize Oracles
-      oracles = [];
-      const requiredAccountsLength = ORACLE_COUNT + 1
-      if (requiredAccountsLength > accounts.length) {
-        throw new Error(`Oracle registration requires access to at least ${requiredAccountsLength} accounts`);
-      }
-      for (let i = 0; i < ORACLE_COUNT; i++) {
-        let newOracle = {};
-        newOracle.address = accounts[i + 1];
-        oracles.push(newOracle);
-      }
 
       console.log("Setting up FlightSuretyApp...");
-      let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
-      // console.log("Flight Surety App = " + JSON.stringify(flightSuretyApp));
-      // Register as an oracle, retrieve indexes
-      console.log("Registering Oracles...");
-      for (let i = 0; i < oracles.length; i++) {
-        flightSuretyApp.methods.registerOracle()
-        .send({ from: oracles[i].address, gas: 1000000, value: REGISTRATION_FEE}, (error, result) => {
-          if (error) {
-            throw new Error("Failed to register oracle. Reason: " + error);
-          }
-        }).then(() => {
-            // Retrieve indexes
-            flightSuretyApp.methods.getOracleIndexes()
-            .call({ from: oracles[i].address}, (error, result) => {
-              if (error) {
-                throw new Error("Failed to call getOracleIndexes()! Reason: " + error);
-              }
-              oracles[i].indexLookup = {};
-              oracles[i].indexLookup[result[0]] = true;
-              oracles[i].indexLookup[result[1]] = true;
-              oracles[i].indexLookup[result[2]] = true;
-            }).then(() => {
-              console.log("Finished setting up oracle " + (i + 1) + " " + JSON.stringify(oracles[i].indexLookup));
-            })
-          })
-      };
+      flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 
-      // Event listener for OracleRequest events
-      flightSuretyApp.events.OracleRequest()
-      .on('data', event => {
-
-        console.log("***** EVENT")
-        // Iterate over oracles and send updates for matches
-        const statusCode = 20;
-        const requestIndex = event.returnValues[0];
-        oracles.map((oracle, index) => {
-          if (oracle.indexLookup[requestIndex] == true) {
-
-            const requestAirline = event.returnValues[1];
-            const requestFlight = event.returnValues[2];
-            const requestTimestamp = event.returnValues[3];
-            const requestIndexAsNumber = parseInt(requestIndex);
-            flightSuretyApp.methods.submitOracleResponse
-            (requestIndexAsNumber, requestAirline,requestFlight, requestTimestamp, statusCode)
-            .send({ from: oracle.address, gas: 100000}, (error, result) => {
-              if (error) {
-                throw new Error("Failed to call submitOracleResponse. Reason: " + error);
-              }
-            });
-          }
-        })
-
-      });
+      console.log("Initializing Oracles service...");
+      oracleService = new OracleService(accounts, flightSuretyApp);
     })
 
   })
+
+
+
+async function registerOracles() {
+  console.log("Registering Oracles...");
+  oracleService.registerOracles();
+}
+
 const app = express();
+app.use(cors);
 app.get('/api', (req, res) => {
     res.send({
       message: 'An API for use with your Dapp!'
     })
+})
+
+app.get('/api/oraclesRegistered', async (req, res) => {
+  console.log("Inside method registerOracles...");
+  res.send({status: oracleService.oraclesRegistered});
+})
+
+
+app.post('/api/registerOracles', async (req, res) => {
+  console.log("Inside method registerOracles...");
+  await registerOracles();
+  res.send({
+    message: "Oracles registered!"
+  })
 })
 
 
